4b7e5afb15d0b4b4718bc3b0fded7fb9930a906b
cen69206 <amorozov@csas.cz>
2023-10-12 16:21:25 +0200

    Scrapy spider impl2

--- ImageDownloader/ImageDownloader/spiders/image_downloader_spider.py
+++ ImageDownloader/ImageDownloader/spiders/image_downloader_spider.py
@@ -1,6 +1,6 @@
 import scrapy
-import json
 import os
+import random
 
 
 class GoogleImagesSpider(scrapy.Spider):
@@ -10,40 +10,26 @@ class GoogleImagesSpider(scrapy.Spider):
         super(GoogleImagesSpider, self).__init__(*args, **kwargs)
         self.query = query
         self.num_images = int(num_images)
+        self.proxies = [
+            'http://37.19.220.129:8443',
+            'http://52.71.249.112:80',
+            'http://138.199.48.1:8443',
+            'http://138.199.48.4:8443',
+            'http://67.217.61.162:80',
+            'http://192.240.106.146:3128',
+            'http://64.189.106.6:3129',
+            'http://107.175.243.183:80',
+            'http://23.152.40.14:3128',
+            'http://144.24.181.61:3128',
+            'http://43.157.10.238:8888',
+            'http://164.215.103.30:3128',
+        ]
 
     def start_requests(self):
         # Google image search URL
         google_url = f'https://www.google.com/search?q={self.query}&tbm=isch'
-
-        # Additional URLs for example
-        additional_urls = [
-            'http://example.com/page1',
-            'http://example.com/page2',
-        ]
-
         # Create requests for Google image search
-        yield scrapy.Request(google_url, self.parse, meta={'proxy': 'http://your_proxy_here'})
-
-        # Create requests for additional URLs
-        for url in additional_urls:
-            yield scrapy.Request(url, self.parse, meta={'proxy': 'http://your_proxy_here'})
-
-    def parse(self, response):
-        # Extract image URLs from JavaScript data
-        page_script = response.xpath('//script[contains(., "_setImagesSrc")]/text()').extract_first()
-        start_idx = page_script.find('AF_initDataCallback(') + len('AF_initDataCallback(')
-        end_idx = page_script.find('}]});') + len('}]});')
-        json_data = json.loads(page_script[start_idx:end_idx])
-        img_urls = [img[-1][0] for img in json_data['afData']]
-
-        # Create folder to save images
-        folder_path = os.path.join(os.path.expanduser('~'), 'Desktop', self.query)
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        # Download images
-        for i, img_url in enumerate(img_urls[:self.num_images]):
-            yield scrapy.Request(img_url, callback=self.save_image, meta={'folder_path': folder_path, 'img_num': i})
+        yield scrapy.Request(google_url, self.parse, meta={'proxy': random.choice(self.proxies)})
 
     def save_image(self, response):
         folder_path = response.meta['folder_path']
