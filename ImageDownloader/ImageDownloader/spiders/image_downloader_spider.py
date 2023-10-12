c9e3b6c8d9442f3309caa0b6d46b3650d957c3f0
silverile <alex.morozovsavs@gmail.com>
2023-10-12 23:04:30 +0200

    Scrapy upgrade

--- ImageDownloader/ImageDownloader/spiders/image_downloader_spider.py
+++ ImageDownloader/ImageDownloader/spiders/image_downloader_spider.py
@@ -1,35 +1,51 @@
+import logging
+
 import scrapy
 import os
-import random
+from urllib.parse import parse_qs, urlparse
 
 
-class GoogleImagesSpider(scrapy.Spider):
-    name = 'google_images'
+class YandexImagesSpider(scrapy.Spider):
+    name = 'yandex_images'
 
     def __init__(self, query='', num_images=5, *args, **kwargs):
-        super(GoogleImagesSpider, self).__init__(*args, **kwargs)
+        super(YandexImagesSpider, self).__init__(*args, **kwargs)
         self.query = query
         self.num_images = int(num_images)
-        self.proxies = [
-            'http://37.19.220.129:8443',
-            'http://52.71.249.112:80',
-            'http://138.199.48.1:8443',
-            'http://138.199.48.4:8443',
-            'http://67.217.61.162:80',
-            'http://192.240.106.146:3128',
-            'http://64.189.106.6:3129',
-            'http://107.175.243.183:80',
-            'http://23.152.40.14:3128',
-            'http://144.24.181.61:3128',
-            'http://43.157.10.238:8888',
-            'http://164.215.103.30:3128',
-        ]
 
     def start_requests(self):
-        # Google image search URL
-        google_url = f'https://www.google.com/search?q={self.query}&tbm=isch'
-        # Create requests for Google image search
-        yield scrapy.Request(google_url, self.parse, meta={'proxy': random.choice(self.proxies)})
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'
+        }
+        yandex_url = f'https://yandex.com/images/search?text={self.query}'
+        yield scrapy.Request(yandex_url, self.parse, headers=headers)
+
+    def parse(self, response):
+        logging.info("Visited %s", response.url)
+
+        # Updated CSS selector to match the new class
+        yandex_urls = response.css('a.serp-item__link::attr(href)').extract()
+        image_urls = []
+
+        for yandex_url in yandex_urls:
+            parsed_url = urlparse(yandex_url)
+            query_params = parse_qs(parsed_url.query)
+            if 'img_url' in query_params:
+                image_url = query_params['img_url'][0]
+                image_urls.append(image_url)
+
+        logging.info("Extracted image URLs: %s", image_urls)
+
+        # Create folder to save images
+        folder_path = os.path.join(os.path.expanduser('~'), 'Desktop', self.query)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            logging.info("Created folder at %s", folder_path)
+
+        # Download images
+        for i, img_url in enumerate(image_urls[:self.num_images]):
+            logging.info("Downloading image %d from URL: %s", i+1, img_url)
+            yield scrapy.Request(img_url, callback=self.save_image, meta={'folder_path': folder_path, 'img_num': i})
 
     def save_image(self, response):
         folder_path = response.meta['folder_path']
@@ -38,3 +54,4 @@ class GoogleImagesSpider(scrapy.Spider):
         img_path = os.path.join(folder_path, img_name)
         with open(img_path, 'wb') as img_file:
             img_file.write(response.body)
+            self.logger.info("Saved image to %s", img_path)
--- ImageDownloader/StartCrawl.py
+++ ImageDownloader/StartCrawl.py
@@ -3,19 +3,24 @@ from threading import Thread
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor
-from ImageDownloader.spiders.image_downloader_spider import GoogleImagesSpider  # Import your spider here
+from ImageDownloader.spiders.image_downloader_spider import YandexImagesSpider  # Import your spider here
 
+
+# scrapy crawl yandex_images -a query="eva elfie" -a num_images=2
 
 def start_crawl(query, num_images):
+    print("Starting crawl")  # Debugging line
     runner = CrawlerRunner(get_project_settings())
-    d = runner.crawl(GoogleImagesSpider, query=query, num_images=num_images)
+    d = runner.crawl(YandexImagesSpider, query=query, num_images=num_images)
     d.addBoth(lambda _: reactor.stop())
     reactor.run(installSignalHandlers=0)  # Disable signal handlers
+    print("Crawl finished")  # Debugging line
 
 
 def on_button_click():
     query = query_entry.get()
     num_images = num_images_entry.get()
+    print(f"Query: {query}, Num Images: {num_images}")  # Debugging line
     t = Thread(target=start_crawl, args=(query, num_images))
     t.start()
 
